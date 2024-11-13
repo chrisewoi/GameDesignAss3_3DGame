7ab72349449f7d492c6acbd85816627ccab76e1b
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2024-11-13 14:44:34 +1100

    trying to add banking

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public static Camera mainCamera;
     public float speed;
+    public float forwardSpeed;
     public ObservedVector3 PlayerDirection;
+    public ObservedTransform PlayerTransform;
     private Vector2 moveInput;
     public bool invertControls;
     private void Awake()
     {
         PlayerDirection.SetReference(transform.forward);
+        PlayerTransform.SetReference(transform);
     }
 
     // Start is called before the first frame update
@@ -30,6 +33,12 @@ public class PlayerMovement : MonoBehaviour
         //transform.position += speed * Time.deltaTime * move;
         transform.forward = transform.forward + transform.right * moveInput.x + transform.up * moveInput.y;
         PlayerDirection.SetReference(transform.forward);
+        transform.forward += transform.forward * forwardSpeed;
+    }
+
+    public Vector2 GetMoveInput()
+    {
+        return moveInput;
     }
 }
 
--- Assets/ShipGraphic.cs
+++ Assets/ShipGraphic.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ShipGraphic : MonoBehaviour
 {
@@ -9,21 +10,53 @@ public class ShipGraphic : MonoBehaviour
     public ObservedTransform ShipTransform;
     public float smoothTime;
     private Vector3 v;
+    
+    public float bankAmount;
+    private PlayerMovement playerMovement;
+    public Vector2 moveInput;
+
+
 
     private void Awake()
     {
         ShipTransform.SetReference(transform);
+        playerMovement = FindObjectOfType<PlayerMovement>();
+
     }
     private void OnEnable()
     {
         ObservedPlayerDirection.Register(PlayerDirection_OnSetReference);
+        ShipTransform.Register(ShipTransform_OnSetReference);
     }
     private void OnDisable()
     {
         ObservedPlayerDirection.Unregister(PlayerDirection_OnSetReference);
+        ShipTransform.Unregister(ShipTransform_OnSetReference);
     }
+
+    private void Update()
+    {
+        Transform ship = ShipTransform.GetReference();
+        moveInput.x = Input.GetAxis("Horizontal");
+        float bankLerp = Mathf.LerpAngle(-bankAmount, bankAmount, (moveInput.x + 1) / 2);
+        Debug.Log($"bank: {bankLerp}");
+        transform.Rotate(0f, 0f, bankLerp);
+        ship = transform;
+        ShipTransform.SetReference(transform);
+    }
+
     public void PlayerDirection_OnSetReference(Vector3 previousReference, Vector3 newReference)
     {
         transform.forward = Vector3.SmoothDamp(transform.forward, newReference, ref v, smoothTime);
     }
+
+    public void ShipTransform_OnSetReference(Transform previousReference, Transform newReference)
+    {
+
+
+        //transform.Rotate(transform.forward * moveInput.x * bankAmount, Space.Self);
+        //transform.forward = Vector3.SmoothDamp(transform.forward, )
+    }
+    
+    
 }
